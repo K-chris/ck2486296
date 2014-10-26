cf51a59ffd1e0e6128d7103847855505f3d4a4cb
kchristopher <kooyman.christopher@gmail.com>
2014-10-26 16:24:43 -0700

    gg

--- Midterm /Problem1.s
+++ Midterm /Problem1.s
@@ -8,7 +8,7 @@ message3: .asciz "your gross pay is $%d   "
 main:
     str lr, [sp,#-8]!
 	sub sp, sp, #8
-    dr r0, address_of_message1
+    ldr r0, address_of_message1
     bl printf
     ldr r0, address_of_format
     mov r2, sp
@@ -17,8 +17,6 @@ main:
     add r1, sp, #4
     ldr r1, [r1]           @r1 = users package a,b,or c
     ldr r2, [sp]           @r2 = users hours
-
-
 
 
 
