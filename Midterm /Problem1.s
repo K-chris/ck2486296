e20ee4adae8ad9feba3eac1b85dba01f82868dc9
kchristopher <kooyman.christopher@gmail.com>
2014-10-26 16:48:09 -0700

    this is easy

--- Midterm /Problem1.s
+++ Midterm /Problem1.s
@@ -1,7 +1,7 @@
 .data
-message1: .asciz "enter in the hours you have worked and then the pay rate"
+message1: .asciz "enter in the hours you have worked and then the pay rate  "
 format: .asciz "%d %d"
-message2: .asciz "You cannot work more than 60 hours, try again "
+message2: .asciz "You cannot work more than 60 hours, try again  "
 message3: .asciz "your gross pay is $%d   "
 .text
 
@@ -9,24 +9,36 @@ message3: .asciz "your gross pay is $%d   "
 main:
     str lr, [sp,#-8]!
 	sub sp, sp, #8
+
  input:
+
     ldr r0, address_of_message1
     bl printf
     ldr r0, address_of_format
     mov r2, sp
-        add r1, r2,#4
+    add r1, r2,#4
     bl scanf
-        add r1, sp, #4
+    add r1, sp, #4
     ldr r1, [r1]           @r1 = users hours
     ldr r2, [sp]           @r2 = users rate of pay
     cmp r1, #60
     bgt case_notlegal
+    cmp r1, #20
+    ble case_regpay
 case_notlegal:
-    ldr r0, address_of_message1
-    bl printf
 
+    ldr r0, address_of_message2
+    bl printf
     b input
+
+case_regpay:
+    mul r3,r1,r2
+    b end
 end:
+
+    mov r1,r3
+    ldr r0, address_of_message3
+    bl printf
     add sp, sp, #+8
     ldr lr, [sp], #+8
     bx lr
