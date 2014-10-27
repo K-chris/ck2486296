84f788b455be158cc5c954f34b8b28a1a68c9e09
kchristopher <kooyman.christopher@gmail.com>
2014-10-27 11:17:30 -0700

    asem

--- Midterm /script.sh
+++ Midterm /script.sh
@@ -7,7 +7,7 @@ gcc -c ${PROGRAM}.s;
 gcc -c ${P1}.s;
 gcc -c ${P2}.s;
 gcc -c ${P3}.s;
-gcc -o ${PROGRAM} ${P1}.o ${P2}.o ${P3}.o ${PROGRAM}.o;
+gcc ${P1}.o ${P2}.o ${P3}.o ${PROGRAM}.o -o ${PROGRAM};
 #gcc -o ${PROGRAM} ${PROGRAM}.o;
 echo "Compiling $PROGRAM..";
 rm -vf *.o
